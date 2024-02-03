344ec836b2fc625ab41e984eb6ade0f07f99ce00
Omar Alhamad <omar93@hotmail.se>
2024-02-03 12:00:45 +0100

    initial commit

---
+++ Datatyper.js
@@ -0,0 +1,20 @@
+/**
+ * Data typer, olika sätt att spara data på inom programmering
+ * olika typer av data bör sparas i rätt data typ, därav finns det
+ * olika datatyper
+ */
+
+
+
+/**
+ * Primitiva datatyper
+ */
+// String | Sträng - Text, ungefär som att klippa ut bokstäver ur en tidning
+// Integer / Int | Heltal, siffror på en miniräknare, går att göra algebra med
+// Boolean | booleskt, En datatyp som antigen har värdet 'true/sant' eller 'false/falskt'
+
+
+/**
+ * Icke Primitiva datatyper
+ */
+// Object | Objekt, En icke primitiv datatype som kan va olika formet
---
+++ Funcktioner.js
@@ -0,0 +1,17 @@
+/**
+ * Funktioner, går att beskriva som en liten maskin eller ett rullband som gör 
+ * ett sätt intruktion, man skulle till exempel skriva en funktion som skriver utt
+ * siffra '3' 14 gånger & sedan skriver ut hej
+ */
+
+
+
+/**
+ * Funktioner
+ */
+
+
+
+/**
+ * Metoder
+ */
